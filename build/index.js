448ae3cb860da1966d4c52cda99ed98cfda9d1de
Genesis Krane <genesiskrane@gmail.com>
2025-07-29 21:58:04 +0100

    Making Build JS

--- build/index.js
+++ build/index.js
@@ -20,4 +20,4 @@ exec('npm install', (error, stdout, stderr) => {
 });
 
 
-console.log(process.env.BUILD_ID ? 'Production Build' : 'Development Build');
+console.log(process.env.BUILD_ID || 'No BUILD_ID set, running in local development mode.');
