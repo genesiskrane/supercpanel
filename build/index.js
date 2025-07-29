4ed4b20d757ec21973970fb3c12d76f599804e98
Genesis Krane <genesiskrane@gmail.com>
2025-07-29 21:55:04 +0100

    Making Build JS

--- build/index.js
+++ build/index.js
@@ -20,4 +20,4 @@ exec('npm install', (error, stdout, stderr) => {
 });
 
 
-console.log(process.env.NODE_ENV === 'production' ? 'Production Build' : 'Development Build');
+console.log(process.env.BUILD_ID ? 'Production Build' : 'Development Build');
