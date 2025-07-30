5669e9e4a9c1a0cccbe087f47b705dba8e665014
Genesis Krane <genesiskrane@gmail.com>
2025-07-30 22:16:50 +0100

    Making Build JS

--- build/index.js
+++ build/index.js
@@ -5,6 +5,33 @@ const axios = require("axios");
 
 const client = new SecretManagerServiceClient();
 
+async function main() {
+  console.info(
+    process.env.IN_CLOUD_BUILD === "true"
+      ? "Production Build"
+      : "Development Build"
+  );
+
+  const secret = await getSecret("SECRET");
+  console.log("Starting build process...");
+  try {
+    const { data } = await axios.get(
+      "https://gkrane.online/api/CP/build?secret=" + secret
+    );
+
+    console.log(data);
+
+    // Build Express Projects
+    console.log("Building Express Projects...");
+
+    for (const project of data.express)
+      buildExpressProject(project.repo, project.id);
+  } catch (error) {
+    console.error(`❌ Error fetching data: ${error.message}`);
+  }
+  console.log("Server Files Built Successfully!");
+}
+
 async function getSecret(secretName) {
   const [version] = await client.accessSecretVersion({
     name: `projects/28739726663/secrets/${secretName}/versions/latest`,
@@ -14,34 +41,13 @@ async function getSecret(secretName) {
   return payload;
 }
 
-const secret = await getSecret("SECRET");
-
-try {
-  const { data } = await axios.get(
-    "https://gkrane.online/api/CP/build?secret=" + secret
-  );
-
-  console.log(data);
-
-  // Build Express Projects
-  console.log("Building Express Projects...");
-
-  for (const project of data.express)
-    buildExpressProject(project.repo, project.id);
-} catch (error) {
-  console.error(`❌ Error fetching data: ${error.message}`);
-}
-
-console.log("Server Files Built Successfully!");
-
-console.log(
-  process.env.IN_CLOUD_BUILD === "true"
-    ? "Production Build"
-    : "Development Build"
-);
-
 function buildExpressProject(repo, id) {
   return new Promise((resolve, reject) => {
     console.log(`Building project ${id} from repo ${repo}...`);
   });
 }
+
+main().catch((error) => {
+  console.error(`❌ Error in main function: ${error.message}`);
+  process.exit(1);
+});
