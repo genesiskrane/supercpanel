8caa9d538056cd1fea8dc526f7e2f9a75e3fc3a3
Genesis Krane <genesiskrane@gmail.com>
2025-08-01 07:29:23 +0100

    Loading Clients

--- core/index.js
+++ core/index.js
@@ -1,11 +1,20 @@
 const axios = require("axios");
 
+const { projects, tokens } = require("../data").getData();
+
 const init = async () => {
   console.log("Initializing Super Cpanel...");
+  console.log(`Loaded ${projects.length} projects`);
 
-  const projects = require("../data").getData().projects;
-
-  console.log(projects);
+  // Build each project's clients to Google Cloud Storage
+  buildClientsToGoogleCloudStorage();
 };
 
+function buildClientsToGoogleCloudStorage() {
+  for (let project of projects) {
+    const { id, clients } = project;
+
+    console.log(id, clients);
+  }
+}
 module.exports = { init };
