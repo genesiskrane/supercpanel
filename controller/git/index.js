d6fe6513ecf1e7966ba45aac60f693423ffaa98c
Augment Chinedu <augmentchinedu@gmail.com>
2025-08-07 17:29:11 +0100

    git URL Added

--- controller/git/index.js
+++ controller/git/index.js
@@ -1,10 +1,11 @@
 const fn = require("../../functions");
 
 const updateDistAfterCommit = (req, res) => {
+  console.log(req.body, req.body.repository.html_url);
 
-    console.log(req.body, req.body.html_url);
-    fn.uploadRepoDist(gitURL, projectID, subname)
-    res.send("Commit endpoint is under construction");
+  let gitURL = req.body.repository.html_url;
+  fn.uploadRepoDist(gitURL, projectID, subname);
+  res.send("Commit endpoint is under construction");
 };
 
 module.exports = { updateDistAfterCommit };
